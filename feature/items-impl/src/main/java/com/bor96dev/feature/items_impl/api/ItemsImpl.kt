a75e4b91f68eadb7b74734032118d36ae18ab13a
jakobakoba <jakobakoba@gmail.com>
2023-06-11 16:43:46 +0600

    added impl

---
+++ feature/items-impl/src/main/java/com/bor96dev/feature/items_impl/api/ItemsImpl.kt
@@ -0,0 +1,10 @@
+package com.bor96dev.feature.items_impl.api
+
+import androidx.fragment.app.Fragment
+import com.bor96dev.feature.items_api.ItemsApi
+import com.bor96dev.feature.items_impl.presentation.ItemsFragment
+
+class ItemsImpl : ItemsApi {
+
+    override fun getFragment(): Fragment = ItemsFragment()
+}
---
+++ feature/items-impl/src/main/java/com/bor96dev/feature/items_impl/presentation/ItemsFragment.kt
@@ -0,0 +1,6 @@
+package com.bor96dev.feature.items_impl.presentation
+
+import androidx.fragment.app.Fragment
+
+internal class ItemsFragment : Fragment() {
+}
